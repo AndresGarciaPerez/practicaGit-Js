886e580da266bffd8474aa37879b94c8f97a9752
AndresGarciaPerez <andygarcia041995@gmail.com>
2025-06-16 12:39:00 -0600

    Se agregaron mas archivos

---
+++ 02-Operadores.js
@@ -0,0 +1,28 @@
+
+// OPERADORES ARITMETICOS
+
+let a = 10, b = 3;
+
+a + b     // 13 (suma)
+a - b     // 7  (resta)
+a * b     // 30 (multiplicación)
+a / b     // 3.333... (división)
+a % b     // 1  (módulo o residuo)
+a ** b    // 1000 (potencia: 10³)
+
+
+//OPERADORES DE COMPARACION
+
+5 == "5"     // true (compara valor, no tipo)
+5 === "5"    // false (compara valor y el tipo)
+5 != "5"     // false ( compara esa declaracion)
+5 !== "5"    // true
+10 > 5       // true
+10 >= 10     // true
+7 < 8        // true
+
+
+//OPERADOR TERNARIO ( FORMA SIMPLE DE IF )
+
+let edad = 20;
+let mensaje = edad >= 18 ? "Mayor de edad" : "Menor de edad";
---
+++ 03-IfElse.js
@@ -0,0 +1,38 @@
+
+// SENTENCIA COMUN
+
+let edad = 20;
+
+if (edad >= 18) {
+  console.log("Eres mayor de edad");
+} else {
+  console.log("Eres menor de edad");
+}
+
+//SENTENCIA SIMPLIFICADA ( TERNARIO )
+
+let edad2 = 20;
+let mensaje = edad >= 18 ? "Mayor de edad" : "Menor de edad";
+
+
+//SENTENCIA IF ANIDADA
+let nota = 75;
+
+if (nota >= 90) {
+  console.log("Excelente");
+} else if (nota >= 70) {
+  console.log("Bueno");
+} else if (nota >= 50) {
+  console.log("Suficiente");
+} else {
+  console.log("Insuficiente");
+}
+
+
+let edadEjercicio = 20;
+
+if(edadEjercicio >= 18){
+  console.log("Puede votar");
+} else {
+  console.log("No puede votar");
+}
